c613b04ab4bc6ce12cf2f11f17515d38e6fe7716
Fernando Martin <fermartingmr@gmail.com>
2022-09-15 17:19:00 +0200

    Ajusted stabline theme

--- lua/statusline-config/init.lua
+++ lua/statusline-config/init.lua
@@ -49,19 +49,26 @@ require('staline').setup {
 }
 
 -- Bufferline
+-- require('stabline').setup {
+-- 	style       = "bar", -- others: arrow, slant, bubble
+-- 	stab_left   = "┃",
+-- 	stab_right  = " ",
+-- 
+-- 	-- fg          = Default is fg of "Normal".
+-- 	-- bg          = Default is bg of "Normal".
+-- 	inactive_bg = "#1e2127",
+-- 	inactive_fg = "#aaaaaa",
+-- 	-- stab_bg     = Default is darker version of bg.,
+-- 
+-- 	font_active = "bold",
+-- 	exclude_fts = { 'NvimTree', 'dashboard', 'lir' },
+-- 	stab_start  = "",   -- The starting of stabline
+-- 	stab_end    = "",
+-- }
 require('stabline').setup {
-	style       = "bar", -- others: arrow, slant, bubble
-	stab_left   = "┃",
-	stab_right  = " ",
-
-	-- fg          = Default is fg of "Normal".
-	-- bg          = Default is bg of "Normal".
-	inactive_bg = "#1e2127",
-	inactive_fg = "#aaaaaa",
-	-- stab_bg     = Default is darker version of bg.,
-
-	font_active = "bold",
-	exclude_fts = { 'NvimTree', 'dashboard', 'lir' },
-	stab_start  = "",   -- The starting of stabline
-	stab_end    = "",
+	style = "slant",
+    inactive_fg = "#364a82",
+	-- bg = "#986fec",
+	-- fg = "#c0caf5",
+	stab_right = "",
 }
