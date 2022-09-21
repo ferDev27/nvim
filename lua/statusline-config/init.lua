4dec0ae16a263df9fe23d73d891a4b7206920d9b
Fernando Martin <fermartingmr@gmail.com>
2022-09-21 19:25:15 +0200

    added staline mac config

--- lua/statusline-config/init.lua
+++ lua/statusline-config/init.lua
@@ -9,35 +9,35 @@ if not ok then
 end
 
 -- Status line
-require "staline".setup {
-	sections = {
-		left = { '  ', 'mode', ' ', 'branch', ' ', 'lsp' },
-		mid = {},
-		right = {'file_name', 'line_column' }
-	},
-	mode_colors = {
-		i = "#9ece6a",
-		ic = "#9ece6a",
-		s = "#9ece6a",
-		S = "#9ece6a",
-		n = "#7aa2f7",
-		c = "#e0af68",
-		v = "#bb9af7",
-		V = "#bb9af7",
-	},
-	defaults = {
-		true_colors = true,
-		line_column = " [%l/%L] :%c  ",
-		--branch_symbol = " "
-        branch_symbol = " "
-	},
-    lsp_symbols = { 
-        Error="  ", 
-        Info="  ", 
-        Warn="  ", 
-        Hint=" " 
-    },
-}
+-- require "staline".setup {
+-- 	sections = {
+-- 		left = { '  ', 'mode', ' ', 'branch', ' ', 'lsp' },
+-- 		mid = {},
+-- 		right = {'file_name', 'line_column' }
+-- 	},
+-- 	mode_colors = {
+-- 		i = "#9ece6a",
+-- 		ic = "#9ece6a",
+-- 		s = "#9ece6a",
+-- 		S = "#9ece6a",
+-- 		n = "#7aa2f7",
+-- 		c = "#e0af68",
+-- 		v = "#bb9af7",
+-- 		V = "#bb9af7",
+-- 	},
+-- 	defaults = {
+-- 		true_colors = true,
+-- 		line_column = " [%l/%L] :%c  ",
+-- 		--branch_symbol = " "
+--         branch_symbol = " "
+-- 	},
+--     lsp_symbols = { 
+--         Error="  ", 
+--         Info="  ", 
+--         Warn="  ", 
+--         Hint=" " 
+--     },
+-- }
 
 -- Buffers
 require('stabline').setup {
@@ -55,4 +55,53 @@ require('stabline').setup {
 	exclude_fts = { 'NvimTree', 'dashboard', 'lir' },
 	stab_start  = "",   -- The starting of stabline
 	stab_end    = "",
+}
+
+--Staline Mac
+require('staline').setup {
+    defaults = {
+        expand_null_ls = false,  -- This expands out all the null-ls sources to be shown
+        left_separator  = "",
+        right_separator = "",
+        full_path       = false,
+        line_column     = "[%l/%L] :%c 並%p%% ", -- `:h stl` to see all flags.
+
+        fg              = "#000000",  -- Foreground text color.
+        bg              = "none",     -- Default background is transparent.
+        inactive_color  = "#364a82",
+        inactive_bgcolor = "none",
+        true_colors     = true,      -- true lsp colors.
+        font_active     = "none",     -- "bold", "italic", "bold,italic", etc
+
+        mod_symbol      = "  ",
+        lsp_client_symbol = " ",
+        branch_symbol   = " ",
+        cool_symbol     = " ",       -- Change this to override default OS icon.
+        null_ls_symbol = "",          -- A symbol to indicate that a source is coming from null-ls
+    },
+ 	mode_colors = {
+ 		i = "#9ece6a",
+ 		ic = "#9ece6a",
+ 		s = "#9ece6a",
+ 		S = "#9ece6a",
+ 		n = "#7aa2f7",
+ 		c = "#e0af68",
+ 		v = "#bb9af7",
+ 		V = "#bb9af7",
+ 	},
+    sections = {
+        left = { '- ', '-mode', 'left_sep_double', ' ', 'branch', ' ', 'lsp' },
+        mid  = { 'file_name' },
+        right = { 'cool_symbol','right_sep_double', '-line_column' },
+    },
+    special_table = {
+        NvimTree = { 'NvimTree', ' ' },
+        packer = { 'Packer',' ' },        -- etc
+    },
+    lsp_symbols = {
+        Error=" ",
+        Info=" ",
+        Warn=" ",
+        Hint=" ",
+    },
 }
