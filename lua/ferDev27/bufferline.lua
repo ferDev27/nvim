71728036949222677791d4f0eb017929377a90fd
Fernando Martin <fermartingmr@gmail.com>
2022-10-02 01:09:12 +0200

    more tweaked bufferline

--- lua/ferDev27/bufferline.lua
+++ lua/ferDev27/bufferline.lua
@@ -17,7 +17,7 @@ bufferline.setup {
         },
         buffer_close_icon = '',
         modified_icon = '●',
-        close_icon = '',
+        --close_icon = '',
         left_trunc_marker = '',
         right_trunc_marker = '',
         max_name_length = 18,
@@ -38,20 +38,13 @@ bufferline.setup {
         show_buffer_icons = true, --| false, -- disable filetype icons for buffers
         show_buffer_close_icons = true, --| false,
         show_buffer_default_icon = true, --| false, -- whether or not an unrecognised filetype should show a default icon
-        show_close_icon = true, --| false,
+        show_close_icon = false, --| false,
         show_tab_indicators = true, --| false,
         show_duplicate_prefix = true, --| false, -- whether to show duplicate buffer prefix
         persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
-        -- can also be a table containing 2 custom separators
-        -- [focused and unfocused]. eg: { '|', '|' }
         separator_style = "thin", --| "thick" | "thin" | { 'any', 'any' },
         enforce_regular_tabs = false, --| true,
         always_show_bufferline = true, --| false,
-        hover = {
-            enabled = true,
-            delay = 200,
-            reveal = {'close'}
-        },
         sort_by = 'insert_at_end', --'insert_after_current' | 'id' | 'extension' | 'relative_directory' | 'directory' | 'tabs' | function(buffer_a, buffer_b)
     },
     highlights = {
